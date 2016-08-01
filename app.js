66d5317aba6bcf9090ed23573e5de1bfad3b6b4e
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-01 13:30:10 -0500

    the return statement

---
+++ app.js
@@ -0,0 +1,15 @@
+/****************************************************
+*               The return Statement                *
+****************************************************/
+function tooeasy() {
+  return "gametime";
+}
+
+document.write(tooeasy() + "<br>");
+
+function addNumbers(a, b) {
+  var c = a + b;
+  return c;
+}
+
+document.write(addNumbers(3,6));
